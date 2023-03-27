e32f852fede5cd62574c862829f3ec7256a38181
Clauvis <ckitieu@ineat.ca>
2023-03-26 21:35:02 -0400

    Add Signup, Signin and Logout

---
+++ Models/Accounts/SignUpModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAdvert.Web.Models.Accounts
+{
+	public class SignUpModel
+	{
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(8, ErrorMessage ="Password must be at least eight characters long!")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Password do not martch")]
+        [Display(Name = "Password")]
+        public string ConfirmPassword { get; set; }
+    
+	}
+}
+
